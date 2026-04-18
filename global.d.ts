91e5c54d9f76e10c6b568001e15b43b210002115
mattuzik <mattuinfo@gmail.com>
2026-04-18 19:21:48 +0300

    Add files via upload

---
+++ global.d.ts
@@ -0,0 +1,38 @@
+declare module "@conarti/eslint-plugin-feature-sliced";
+declare module "*.svg";
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.gif";
+declare module "*.webp";
+declare module "*.avif";
+declare module "*.ico";
+
+declare module "*.mp4" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.webm";
+declare module "*.ogg";
+declare module "*.mp3";
+declare module "*.wav";
+declare module "*.flac";
+declare module "*.aac";
+
+declare module "*.css";
+declare module "*.scss";
+declare module "*.sass";
+declare module "*.less";
+
+declare module "*.woff";
+declare module "*.woff2";
+declare module "*.eot";
+declare module "*.ttf";
+declare module "*.otf";
+
+declare module "*.json";
+declare module "*.wasm";
+
+declare module "*?url";
+declare module "*?raw";
---
+++ vite-env.d.ts
@@ -0,0 +1,14 @@
+/// <reference types="vite/client" />
+declare module "*.svg";
+
+declare module "*.svg" {
+  import * as React from 'react';
+  export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}
+
+declare module "@/assets/*" {
+  const content: any;
+  export default content;
+}
