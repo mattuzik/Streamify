4d9cb3ef869823cd4dad26e2de6c2b507d643a64
mattuzik <mattuinfo@gmail.com>
2026-04-18 19:22:02 +0300

    Update vite-env.d.ts

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,13 +1,6 @@
 /// <reference types="vite/client" />
 declare module "*.svg";
 
-declare module "*.svg" {
-  import * as React from 'react';
-  export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
-  const src: string;
-  export default src;
-}
-
 declare module "@/assets/*" {
   const content: any;
   export default content;
